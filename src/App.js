0cbae4f9d3b47cc6fbe8b9681f7131514062fd80
dsc-akgec <57393186+dsc-akgec@users.noreply.github.com>
2020-08-24 10:57:41 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
-import Nav from './Nav.js';
+import Nav from './Nav';
+import Home from './Home';
+import About from './About'
+import Events from './Events'
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import IconButton from '@material-ui/core/IconButton'
+import Project from './Project';
+import Team from './Team'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      <Home/>
+      <IconButton id="icon-button">
+      <ArrowDownwardIcon  style={{fontSize:50}} />
+      </IconButton>
+      <About/>
+      <Events/>
+      <Project/>
+      <Team/>
+      {/* <Footer/> */}
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Nav.css'
 import logo from './Images/footerLogo.png'
 
-export class Nav extends Component {
-    render() {
-        return (
-            <div className="nav-bar">
-                <img className="nav-bar-logo" src={logo}></img>
-                <ul className="nav-bar-list">
-                    <a href="">HOME</a>
-                    <a href="">ABOUT</a>
-                    <a href="">EVENT</a>
-                    <a href="">PROJECTS</a>
-                    <a href="">TEAM</a>
-                </ul>
-            </div>
-        )
-    }
+// 
+// complete
+// 
+export default function Nav() {
+    return (
+    <div className="nav-bar">
+        <img className="nav-bar-logo" src={logo}></img>
+        <ul className="nav-bar-list">
+            <a href="#" className="nav-bar-home" style={{borderBottom:"0.4vw solid #4285F4"}}>HOME</a>
+            <a href="#main-about" classname="nav-bar-about" style={{borderBottom:"0.4vw solid #EA4335"}}>ABOUT</a>
+            <a href="#main-event" className="nav-bar-event" style={{borderBottom:"0.4vw solid #FBBC04"}}>EVENT</a>
+            <a href="#main-project" className="nav-bar-projects" style={{borderBottom:"0.4vw solid #34A853"}}>PROJECTS</a>
+            <a href="#main-team" className="nav-bar-team" style={{borderBottom:"0.4vw solid #5F6368"}}>TEAM</a>
+        </ul>
+    </div>
+    )
 }
-
-export default Nav
